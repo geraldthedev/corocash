97cefd812d9cafd16a1f5a93c692f3b8924cdbb1
Gerald <hakeem1106@gmail.com>
2020-04-12 02:52:35 -0400

    building model. Will have data from API parse to model to store in database

---
+++ server/models/saveRates.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const priceSchema = new mongoose.Schema({
+    currency: 'string',
+    value: 'integer'
+});
+const Prices = mongoose.model('Prices', priceSchema);
+
+const rate = new Prices({
+    currency: Object.keys(res.data.rates),
+    value: Object.values(res.data.rates)
+})
+
