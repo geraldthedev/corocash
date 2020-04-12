9b35cfaf44f91f2a138d45ebec43733824fa1480
Gerald Bryant <hakeem1106@gmail.com>
2020-04-12 23:01:24 +0000

    adding logic

--- server/models/saveRates.js
+++ server/models/saveRates.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose')
+const express = require('express')
+const router = express.Router()
+
 const priceSchema = new mongoose.Schema({
+    update:'integer',
     currency: 'string',
     value: 'integer'
 });
 const Prices = mongoose.model('Prices', priceSchema);
 
+
 const rate = new Prices({
+    update: res.data.timestamp,
     currency: Object.keys(res.data.rates),
     value: Object.values(res.data.rates)
 })
+
 
 
 rate.save((err)=>{
