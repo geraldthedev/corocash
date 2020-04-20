3a956976f0763c566e10385a5231dc649b760ac5
Gerald Bryant <hakeem1106@gmail.com>
2020-04-20 19:04:24 -0400

    schema update

--- server/models/saveRates.js
+++ server/models/saveRates.js
@@ -4,18 +4,24 @@ const rateRoute = express.Router()
 const dbconnect = require('../middleware/dbconnect')
 
 const priceSchema = new mongoose.Schema({
-    update:'integer',
-    currency: 'string',
-    value: 'float'
+    "update": {
+        type: Number
+    },
+    "currency": {
+        type: String
+    },
+    "value": {
+        type: Number
+    }
 });
 const Prices = mongoose.model('Prices', priceSchema);
 
 
 rateRoute.get('/',(req, res, next)=>{
     const rate = new Prices({
-        update: res.data.timestamp,
-        currency: Object.keys(res.data.rates),
-        value: Object.values(res.data.rates)
+        "update": res.data.timestamp,
+        "currency": Object.keys(res.data.rates),
+        "value": Object.values(res.data.rates)
     })
 
 
